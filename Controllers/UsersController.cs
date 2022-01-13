fc5ec08db75b1d5ecc40b2fb5c46e63171ae3f60
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2022-01-13 12:40:25 +0100

    Dodaj pliki projektów.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DatingApp.Data;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //get
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.AppUsers.ToListAsync(); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int? id)
+        {
+            return await _context.AppUsers.FindAsync(id);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
