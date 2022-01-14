78b16a47d98fea1b9a6cc77a6706d97f8f3293ec
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2022-01-14 18:21:20 +0100

    nxt

--- DatingAppAngular/src/app/app.component.ts
+++ DatingAppAngular/src/app/app.component.ts
@@ -8,11 +8,18 @@ import { HttpClient } from '@angular/common/http'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  title = 'Dating App';
+  users: any;
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-      throw new Error('Method not implemented.');
+  ngOnInit() {
+    this.getUsers();
   }
-  title = 'Dating App';
+  getUsers() {
+    this.http.get('https://localhost:5001/api/Users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
